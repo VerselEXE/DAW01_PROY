8af2bf2063c0fde6ad2d8321c4027554c582c7c3
VerselEXE <I202123660@cibertec.edu.pe>
2023-11-15 12:15:43 -0500

    primer commit

---
+++ proyectoDAW01/src/main/java/org/cibertec/edu/pe/ProyectoDaw01Application.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDaw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDaw01Application.class, args);
+	}
+
+}
---
+++ proyectoDAW01/src/main/java/org/cibertec/edu/pe/model/Producto.java
@@ -0,0 +1,91 @@
+package org.cibertec.edu.pe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="producto")
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idProd;
+	private int idCat;	
+	private String nombreProd;
+	private double precioProd;
+	private int stockProd;
+	private String descProd;
+	private String imagen;
+	
+	public Producto() {		
+	}
+
+	public Producto(int idProd, int idCat, String nombreProd, double precioProd, int stockProd, String descProd,
+			String imagen) {
+		this.idProd = idProd;
+		this.idCat = idCat;
+		this.nombreProd = nombreProd;
+		this.precioProd = precioProd;
+		this.stockProd = stockProd;
+		this.descProd = descProd;
+		this.imagen = imagen;
+	}
+
+	public int getIdProd() {
+		return idProd;
+	}
+
+	public void setIdProd(int idProd) {
+		this.idProd = idProd;
+	}
+
+	public int getIdCat() {
+		return idCat;
+	}
+
+	public void setIdCat(int idCat) {
+		this.idCat = idCat;
+	}
+
+	public String getNombreProd() {
+		return nombreProd;
+	}
+
+	public void setNombreProd(String nombreProd) {
+		this.nombreProd = nombreProd;
+	}
+
+	public double getPrecioProd() {
+		return precioProd;
+	}
+
+	public void setPrecioProd(double precioProd) {
+		this.precioProd = precioProd;
+	}
+
+	public int getStockProd() {
+		return stockProd;
+	}
+
+	public void setStockProd(int stockProd) {
+		this.stockProd = stockProd;
+	}
+
+	public String getDescProd() {
+		return descProd;
+	}
+
+	public void setDescProd(String descProd) {
+		this.descProd = descProd;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}		
+}
